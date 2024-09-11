a7f1e2ee499831e92fbf55e74192e763e2271f90
Bhaskar Thakur <bhaskart155@gmail.com>
2024-09-11 19:11:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
 
 # Write directly to the app
 st.title("Example Streamlit App :balloon:")
@@ -12,7 +13,6 @@ st.write(
     """
 )
 
-from snowflake.snowpark.functions import col
 
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -40,3 +40,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!', icon="✅")
+
+cnx = st.connection("snowflake)
+                    session = cnx.sesssion()
