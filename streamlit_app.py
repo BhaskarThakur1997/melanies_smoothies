59ec0fd7fd212229d6bed37a45ce61a2f2f975c3
Bhaskar Thakur <bhaskart155@gmail.com>
2024-09-12 10:26:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,9 +34,7 @@ session = create_session()
 # Use session to query table
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
-session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+
 
 ingredients_list = st.multiselect( 
 'Choose up to 5 ingredients:' 
