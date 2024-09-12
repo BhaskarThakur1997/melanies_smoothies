42a99696a973e269c756fa86c97da8a94fb779e0
Bhaskar Thakur <bhaskart155@gmail.com>
2024-09-12 10:17:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 st.text(fruityvice_response)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon") 
-#st.text(fruityvice_response.json())
+
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
                     
